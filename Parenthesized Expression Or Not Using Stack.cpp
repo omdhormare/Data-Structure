524cc2b652fd8718b36d59aa681c5efb6b7c61cd
Om Dhormare <143721889+omdhormare@users.noreply.github.com>
2023-09-02 21:14:35 +0530

    Add files via upload

---
+++ Parenthesized Expression Or Not Using Stack.cpp
@@ -0,0 +1,95 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+#define MAX 100
+
+struct stack
+{
+    char a[MAX];
+    int top;
+} s1;
+
+void init()
+{
+    s1.top = -1; // Assign -1 to indicate an empty stack
+}
+
+int isempty()
+{
+    if (s1.top == -1)
+        return 1;
+    else
+        return 0;
+}
+
+int isfull()
+{
+    if (s1.top == MAX - 1)
+        return 1;
+    else
+        return 0;
+}
+
+void push(char c)
+{
+    if (isfull())
+        printf("\nStack is Full. Cannot Push...");
+    else
+    {
+        s1.top++;
+        s1.a[s1.top] = c;
+    }
+}
+
+char pop()
+{
+    char ch;
+    if (isempty())
+        return '\0'; // Return null character to indicate an empty stack
+    else
+    {
+        ch = s1.a[s1.top];
+        s1.top--;
+        return ch;
+    }
+}
+
+int main()
+{
+    char e[100];
+    init();
+    int i;
+    printf("Enter The Expression: ");
+    scanf("%s",&e);
+    for (i = 0; e[i] != '\0'; i++)
+    {
+        if (e[i] == '[' || e[i] == '{' || e[i] =='(')
+        {
+            push(e[i]);
+        }
+        else if (e[i] == ']' || e[i] == '}' || e[i] == ')')
+        {
+            if (isempty())
+            {
+                printf("\nExpression is Not Parenthesized....");
+                return 0;
+            }
+            char top_char = pop();
+            if ((e[i] == ']' && top_char != '[') ||
+                (e[i] == '}' && top_char != '{') ||
+                (e[i] == ')' && top_char != '('))
+            {
+                printf("\nExpression is Not Parenthesized....");
+                return 0;
+            }
+        }
+    }
+
+    if (isempty())
+        printf("\nExpression is Parenthesized....");
+    else
+        printf("\nExpression is Not Parenthesized....");
+
+    return 0;
+}
+
